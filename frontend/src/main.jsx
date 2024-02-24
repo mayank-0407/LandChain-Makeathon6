ae190c4f4e36e54d77e5d91d85621b4d4b108dfc
SAMIVo007 <91935809+SAMIVo007@users.noreply.github.com>
2024-02-24 23:59:45 +0530

    updating frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,40 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Signup from "./Pages/Signup.jsx";
-import Layout from "./Layout.jsx";
-import Home from "./Pages/Home.jsx";
-import Login from "./Pages/Login.jsx";
-import AddVehicle from "./Pages/Dashboard/AddVehicle.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "dashboard",
-        element: <AddVehicle />,
-      },
-    ],
-  },
-]);
+import LandProvider from "./context/LandContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<LandProvider>
+			<App />
+		</LandProvider>
+	</React.StrictMode>
 );
